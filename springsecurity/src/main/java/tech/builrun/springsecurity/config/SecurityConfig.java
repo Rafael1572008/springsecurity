6823d3e4511cc35271c8c613fd9dbaa80f0d386d
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2025-12-06 14:22:18 -0300

    feat(Encoder and Decooder Jwt)

---
+++ springsecurity/src/main/java/tech/builrun/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package tech.builrun.springsecurity.config;
+
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.shaded.jcip.Immutable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+/// Aplicar configurações ‘web’ de segurança
+@EnableWebSecurity
+
+/// Definições de beans
+@Configuration
+public class SecurityConfig {
+
+    @Value("$[jwtpublic.key]")
+    private RSAPublicKey publicKey;
+
+    @Value("$[jwtprivate.key]")
+    private RSAPrivateKey privateKey;
+
+
+
+
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+
+            /// Para qualquer requisição, necessário o ‘token’
+            authorizationManagerRequestMatcherRegistry.anyRequest().authenticated()
+
+        ).csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+
+                /// Configurar aplicação com o oauth (espera receber access ‘tokens’ para autorizar chamadas)
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+
+
+                /// Aplicação não vai usar nem criar http 'session'
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+    /// Descriptografia
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    /// Descriptografia
+    @Bean
+    public JwtEncoder jwtEncoder(){
+
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+}
